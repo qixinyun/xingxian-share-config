406ab6f066f2cb0530a100a9635050e7c6c03107
wangdan <1145557631@qq.com>
2021-12-30 19:43:17 +0800

    #200537-msg-兴县项目初始化

--- src/SmartyConfig/Common/SmartyConfig.php
+++ src/SmartyConfig/Common/SmartyConfig.php
@@ -6,9 +6,9 @@ class SmartyConfig
     public static function configDir()
     {
         $dirArray = array(
-            S_ROOT.'vendor/qixinyun/lanxian-share-config/src/SmartyConfig/Lx',
-            S_ROOT.'vendor/qixinyun/lanxian-share-config/src/SmartyConfig/Common',
-            S_ROOT.'vendor/qixinyun/lanxian-share-config/src/SmartyConfig/',
+            S_ROOT.'vendor/qixinyun/xingxian-share-config/src/SmartyConfig/Xx',
+            S_ROOT.'vendor/qixinyun/xingxian-share-config/src/SmartyConfig/Common',
+            S_ROOT.'vendor/qixinyun/xingxian-share-config/src/SmartyConfig/',
         );
 
         return $dirArray;
